06b33961123a58f4700d67d20cd8198d57547f87
gileswang <gileswang@gmail.com>
2017-07-20 09:25:08 +0800

    Add Log Module

---
+++ WJOP.Foundation.Log/SDK/Tag.cs
@@ -0,0 +1,77 @@
+﻿using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WJOP.Foundation.Log.SDK
+{
+    [ProtoContract]
+    public class Tag
+    {
+        [ProtoMember(3)]
+        private float? FloatValue
+        {
+            get;
+            set;
+        }
+
+        [ProtoMember(1)]
+        private int? IntValue
+        {
+            get;
+            set;
+        }
+
+        [ProtoMember(2)]
+        private string StringValue
+        {
+            get;
+            set;
+        }
+
+        internal object Dump()
+        {
+            object stringValue;
+            if (!string.IsNullOrEmpty(this.StringValue))
+            {
+                stringValue = this.StringValue;
+            }
+            else if (!this.IntValue.HasValue)
+            {
+                stringValue = !this.FloatValue.HasValue ? null : this.FloatValue;
+            }
+            else
+            {
+                stringValue = this.IntValue.Value;
+            }
+            return stringValue;
+
+        }
+
+        public static implicit operator Tag(string value)
+        {
+            return new Tag()
+            {
+                StringValue = value
+            };
+        }
+
+        public static implicit operator Tag(int value)
+        {
+            return new Tag()
+            {
+                IntValue = new int?(value)
+            };
+        }
+
+        public static implicit operator Tag(float value)
+        {
+            return new Tag()
+            {
+                FloatValue = new float?(value)
+            };
+        }
+    }
+}
